5e62a66d0c284e3404096382bf930ba4aca2190b
Nel-ctrl <nflordel@my.centennialcollege.ca>
2025-03-23 01:42:23 -0400

    A lot of gradle work done - the app boots
    - also added singleton pattern within the ProductDatabsae
    -am gonna cry

--- NeilCharles_COMP304Section003_Lab3/app/src/main/java/com/charles/neilcharles_comp304section003_lab3/ui/screens/ProductListScreen.kt
+++ NeilCharles_COMP304Section003_Lab3/app/src/main/java/com/charles/neilcharles_comp304section003_lab3/ui/screens/ProductListScreen.kt
@@ -7,26 +7,37 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.*
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import com.charles.neilcharles_comp304section003_lab3.ui.viewmodels.ProductViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.charles.neilcharles_comp304section003_lab3.models.Product
-
+import com.charles.neilcharles_comp304section003_lab3.repository.ProductRepository
+import androidx.compose.runtime.collectAsState
 
 @Composable
-fun ProductListScreen(productViewModel: ProductViewModel =  viewModel()){
-    val products = productViewModel.allProducts
+fun ProductListScreen(repository: ProductRepository){
+    val productViewModel: ProductViewModel = viewModel(
+        factory = object: ViewModelProvider.Factory {
+            override fun <T: ViewModel>create(modelClass: Class<T>): T {
+                return ProductViewModel(repository) as T
+            }
+        }
+    )
 
-    Column(modifier = Modifier.fillMaxSize().padding(16.dp)){
+    val products = productViewModel.allProducts.collectAsState(initial = emptyList()).value
+
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
         Text(text = "Product List", style = MaterialTheme.typography.headlineMedium)
 
         LazyColumn(modifier = Modifier.fillMaxSize()) {
-            items(products){ product ->
+            items(products) { product ->
                 ProductItem(product)
-
             }
         }
     }
 }
+
 @Composable
 fun ProductItem(product: Product) {
     Card(
