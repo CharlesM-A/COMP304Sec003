0dbf7707d4cd439fdb3ba8a905b571dd0277823b
Nel-ctrl <nflordel@my.centennialcollege.ca>
2025-03-23 21:32:57 -0400

    Added Sample Database

--- NeilCharles_COMP304Section003_Lab3/app/src/main/java/com/charles/neilcharles_comp304section003_lab3/MainActivity.kt
+++ NeilCharles_COMP304Section003_Lab3/app/src/main/java/com/charles/neilcharles_comp304section003_lab3/MainActivity.kt
@@ -4,17 +4,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.collectAsState
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.charles.neilcharles_comp304section003_lab3.database.ProductDatabase
+import com.charles.neilcharles_comp304section003_lab3.models.Product
 import com.charles.neilcharles_comp304section003_lab3.repository.ProductRepository
 import com.charles.neilcharles_comp304section003_lab3.ui.screens.AddProductScreen
 import com.charles.neilcharles_comp304section003_lab3.ui.screens.ProductListScreen
 import com.charles.neilcharles_comp304section003_lab3.ui.theme.NeilCharles_COMP304Section003_Lab3Theme
 import com.charles.neilcharles_comp304section003_lab3.ui.viewmodels.ProductViewModel
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
@@ -27,6 +31,11 @@ class MainActivity : ComponentActivity() {
 
         // Manually create ProductViewModel (no ViewModelProvider needed)
         val productViewModel = ProductViewModel(repository)
+
+        // Initialize database with sample data
+        lifecycleScope.launch {
+            ProductDbInitializer(database).populateDatabase()
+        }
 
         setContent {
             val navController = rememberNavController()
@@ -58,4 +67,43 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
+    //Sample data
+    class ProductDbInitializer(private val database: ProductDatabase) {
+        private val dao = database.productDao()
+
+        suspend fun populateDatabase() {
+            // Check if database is empty first
+            if (dao.getAllProducts().first().isEmpty()) {
+                val initialProducts = listOf(
+                    Product(
+                        prodID = 101,
+                        prodName = "IPhone 12 Mini",
+                        prodPrice = 799.99,
+                        dateOfDelivery = "2025-03-25",
+                        prodCategory = "Cell Phone",
+                        prodFavourites = true
+                    ),
+                    Product(
+                        prodID = 102,
+                        prodName = "ASUS TUF Gaming A15(2024) Gaming Laptop ",
+                        prodPrice = 1799.99,
+                        dateOfDelivery = "2025-03-26",
+                        prodCategory = "Electronics",
+                        prodFavourites = false
+                    ),
+                    Product(
+                        prodID = 103,
+                        prodName = "Sony Bravia 8 77'in 4k UDH HDR OLED Smart Google TV",
+                        prodPrice = 3799.99,
+                        dateOfDelivery = "2025-03-30",
+                        prodCategory = "Media",
+                        prodFavourites = false
+                    )
+                )
+                initialProducts.forEach { dao.upsertProduct(it) }
+            }
+        }
+    }
 }
+
